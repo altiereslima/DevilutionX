4856d1024020109e18ff4a0fa1045cbf04d6fce6
altiereslima <altieres.lima@gmail.com>
2025-04-14 18:51:11 -0300

    Update position.hpp

--- Source/engine/position.hpp
+++ Source/engine/position.hpp
@@ -1,43 +1,17 @@
 #pragma once
 
-#include "engine/displacement.hpp"
+#include "engine/point.hpp"
 
 namespace devilution {
 
 struct Position {
     Point tile;      // Tile position (grid coordinates)
     Point offset;    // Sub-tile offset in pixels
-
-    void UpdatePosition() {
-        // Handle sub-tile offset overflow
-        while (offset.x >= TileWidth) {
-            offset.x -= TileWidth;
-            tile.x++;
-        }
-        while (offset.x < 0) {
-            offset.x += TileWidth;
-            tile.x--;
-        }
-        while (offset.y >= TileHeight) {
-            offset.y -= TileHeight;
-            tile.y++;
-        }
-        while (offset.y < 0) {
-            offset.y += TileHeight;
-            tile.y--;
-        }
-    }
 };
 
-struct ActorPosition {
-    Position position;
-    Point velocity;
-
-    void Update() {
-        position.offset.x += velocity.x;
-        position.offset.y += velocity.y;
-        position.UpdatePosition();
-    }
+struct MovementSettings {
+    int speed;       // Movement speed in pixels
+    bool smooth;     // Whether to use smooth movement
 };
 
 } // namespace devilution
